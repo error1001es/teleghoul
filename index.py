975f0a71b766bc955c370c88b957226f946064ef
= <error1001es@gmail.com>
2021-06-15 00:20:20 +0600

    add new commands and fixes old

--- index.py
+++ index.py
@@ -1,26 +1,153 @@
+import asyncio
 from config import *
 from time import sleep
-from pyrogram  import Client, filters, sync
+from pyrogram import Client, filters, sync
+from pyrogram.errors import FloodWait
 
 client = Client('ghoul-session', api_id, api_hash)
 
-client.start()
+limit = 2000
 
-me = client.get_me()
+class custom(dict):
+    def __missing__(self, key):
+        return 0
+
+client.start()
 
 client.stop()
 
 print('Бот запущен, теперь ты - Гуль')
 
-@client.on_message(filters.text)
-async def normal_handler(client, message):
-    if(message.text.lower() == start_command and message.from_user.id == me.id):
-        i = 1000
-        while i > 0:
-            sleep(1/messages_per_second)
-            await client.send_message(message.chat.id, str(i)+' - 7 = '+str(i-7))
-            i -= 7
-            if(end_message != ''):
-                await client.send_message(message.chat.id, end_message)  
+
+@client.on_message(filters.command('я_гуль', prefixes=['/', '!', '.']) & filters.me)
+def ghoul_handler(client, message):
+    i = 1000
+    while i > 0:
+        try:
+            client.send_message(message.chat.id, str(i)+' - 7 = '+str(i-7))
+        except FloodWait as e:
+            sleep(e.x)
+
+        i -= 7
+        sleep(1/messages_per_second)        
+
+    if(end_message != ''):
+        client.send_message(message.chat.id, end_message)
+
+
+@client.on_message(filters.command('я гуль', prefixes=['/', '!', '.']) & filters.me)
+def ghoul_handler(client, message):
+    i = 1000
+    while i > 0:
+        try:
+            message.edit_text(str(i)+' - 7 = '+str(i-7))
+        except FloodWait as e:
+            sleep(e.x)
+        i -= 7    
+        sleep(sleep_time)        
+
+    if(end_message != ''):
+        client.send_message(message.chat.id, end_message)
+
+
+@client.on_message(filters.command('words', prefixes=['/', '!', '.']) & filters.me)
+def words_handler(client, message):
+    words = custom()
+    total = 0
+    progress = client.send_message(message.chat.id, '`Загружено 0 сообщений...`')
+    for message in client.iter_history(message.chat.id, limit):
+        if(message.text):
+            splited_text = message.text.split()
+        if('Счетчик_слов' not in splited_text and '/words' not in splited_text):
+            total += 1
+            if total % 200 == 0:
+                progress.edit_text(f'`Загружено {total} сообщений...`')
+            for word in splited_text:
+                words[word.lower()] += 1
+
+    frequency = sorted(words, key=words.get, reverse=True)
+    out = 'Счетчик_слов\n'
+    for i in range(10):
+        out += f'{i+1}. {frequency[i]} - {words[frequency[i]]}\n'
+
+    progress.edit_text(out, parse_mode=None)
+
+
+@client.on_message(filters.command('type', prefixes=['/', '!', '.']) & filters.me)
+def type_handler(_, message):
+    overriding_text = message.text.split('.type ', maxsplit=1)[1]
+    text = overriding_text
+    final_message = ''
+    typing_symbol = '▒'
+ 
+    while(final_message != overriding_text):
+        try:
+            message.edit(final_message + typing_symbol)
+            sleep(sleep_time)
+ 
+            final_message = final_message + text[0]
+            text = text[1:]
+ 
+            message.edit(final_message)
+            sleep(sleep_time)
+ 
+        except FloodWait as e:
+            sleep(e.x)                    
+
+
+@client.on_message(filters.command('music', prefixes=['/', '!', '.']) & filters.me)
+async def music_handler(_, message):
+    try:
+        cmd = message.command
+
+        song_name = ''
+        if len(cmd) > 1:
+            song_name = ' '.join(cmd[1:])
+        elif message.reply_to_message and len(cmd) == 1:
+            song_name = (
+                message.reply_to_message.text or message.reply_to_message.caption
+            )
+        elif not message.reply_to_message and len(cmd) == 1:
+            await message.edit('Give a song name')
+            await asyncio.sleep(2)
+            await message.delete()
+            return
+
+        song_results = await client.get_inline_bot_results('deezermusicbot', song_name)
+
+        try:
+            saved = await client.send_inline_bot_result(
+                chat_id='me',
+                query_id=song_results.query_id,
+                result_id=song_results.results[0].id,
+                hide_via=True,
+            )
+
+            # Пересылаем сообщение в избранное, как новое
+            saved = await client.get_messages('me', int(saved.updates[1].message.id))
+            reply_to = (
+                message.reply_to_message.message_id
+                if message.reply_to_message
+                else None
+            )
+            await client.send_audio(
+                chat_id=message.chat.id,
+                audio=str(saved.audio.file_id),
+                reply_to_message_id=reply_to,
+            )
+
+            # Удаляем сообщение из избранного
+            await client.delete_messages('me', saved.message_id)
+        except TimeoutError:
+            await message.edit('That didn\'t work out')
+            await asyncio.sleep(2)
+        await message.delete()
+    except Exception as e:
+        print(e)
+        await message.edit('`Failed to find song`')
+        await asyncio.sleep(2)
+        await message.delete()
+
+
 
 client.run()
