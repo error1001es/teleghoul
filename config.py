d2479352c83fc0679ac38ce72e678536cf082221
error1001es <49606492+error1001es@users.noreply.github.com>
2021-06-15 18:12:31 +0600

    Update config.py

--- config.py
+++ config.py
@@ -1,18 +1,22 @@
 # Конфиг
+command_prefixes = ['.','!','/']
+
 ghoul_spam_command = 'ghoul-spam'
 ghoul_table_command = 'ghoul-c'
 ghoul_command = 'ghoul'
-music_command = 'music'
-type_command = 'type'
-words_command = 'words'
 
 end_message = 'l l let me die' # Сообщение после конца цикла, если не нужно - оставляем пустым
 messages_per_second = 7 # Для ghoul_spam
 sleep_time_ghoul = 0.0825 # Для ghoul_table
 sleep_time = 0.055 # Для type
 
+# Команды снизу доступны только тем, кто пожертвовал
+music_command = 'music'
+type_command = 'type'
+words_command = 'words'
 
-command_prefixes = ['.','!','/']
+
+
 
 
 # Линии ниже не трогать, сломается!
