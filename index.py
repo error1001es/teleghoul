bb1d415bff76b671daa33c464009fd95949c0bdf
error1001es <49606492+error1001es@users.noreply.github.com>
2021-07-17 20:21:29 +0600

    Update index.py

--- index.py
+++ index.py
@@ -27,4 +27,24 @@ def ghoul_spam_handler(client, message):
     if(end_message != ''):
         client.send_message(message.chat.id, end_message)
 
+
+@client.on_message(filters.command('ghoul-c', prefixes=['.','!','/']) & filters.me)
+def ghoul_table_handler(client, message):
+    i = 1000
+    while i > 62:
+        try:
+            text = f'{i} - 7 = {i-7}'
+            for j in range(1,10):
+                text += f'\n{i-7*j} - 7 = {i-7*(j+1)}'
+            message.edit_text(f'`{text}`')
+            sleep(sleep_time_ghoul)
+        except FloodWait as e:
+            sleep(e.x)
+
+        i -= 7
+
+    if(end_message != ''):
+        client.send_message(message.chat.id, end_message)
+
+
 client.run()
